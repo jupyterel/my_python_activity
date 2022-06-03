21499a21e74e87530473ce53425710a40895cdd4
jupyterel <102231794+jupyterel@users.noreply.github.com>
2022-06-03 12:36:25 +0200

    Add files via upload

---
+++ my_first_code.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+
+print("My name is Dr Eleonora Bacchi")
+print("The date is 3/6/2022")
+
+
+# In[5]:
+
+
+# Write a program to analyse real estate data
+
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+# Total sales for December
+
+tot_sales = a + b + c + d + e
+print(tot_sales)
+
+
+# In[7]:
+
+
+# Average sales price
+
+avg_sales = tot_sales / 5
+
+print(avg_sales)
+
+
+# In[8]:
+
+
+# C price
+
+c_price = 67000 - 6500
+
+print(c_price)
+
+
+# In[ ]:
+
+
+
+
